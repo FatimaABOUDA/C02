e57e89f5564ab8bf2bfe65bc4bd683b8b7da0e05
FatimaABOUDA <122871593+FatimaABOUDA@users.noreply.github.com>
2023-02-28 10:32:43 +0100

    Initial commit

--- src/Exercice/exercice1.java
+++ src/Exercice/exercice1.java
@@ -5,7 +5,7 @@ public class exercice1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nb1=10;
-        int nb2=-6;
+        int nb2=6;
         boolean a = nb1 > nb2;
         boolean b=nb1==nb2;
         boolean c= nb1<nb2+50;
